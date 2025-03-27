5a2a116e7708e88933ebe0f7573e0bb549a7e9ef
Tushar Chokshi <tushar239@yahoo.com>
2025-03-27 05:47:16 -0700

    first commit

--- models.py
+++ models.py
@@ -1,26 +1,6 @@
 
-from flask import Flask
-# when I tried to install flask_sqlachemy using pip install flask-sqlalchemy
-# it installed in global python library (D:\Projects\Python312\Lib\site-packages) instead of workspace's virtual env(.venv)
-# I am not sure why did it do like that
-# But now, I have to use global python library as my Python Interpreter instead of venv
-# So, ctrl+shift+p, Python: Select Interpreter, choose Global interpreter which is installed at D:\Projects\Python312
-from flask_sqlalchemy import SQLAlchemy
+from init_database import app, db, engine
 from datetime import datetime
-
-# initialize an app
-app = Flask(__name__)
-
-# to work with mysql, you need to follow https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application
-# DATABASE_URI = "sqlite:///todo.db"
-DATABASE_URI = "mysql://admin:admin@localhost:3306/todo"
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-db = SQLAlchemy(app)
-engine = db.create_engine(DATABASE_URI)
 
 
 # ORM in flask - Declare a mapping
@@ -36,6 +16,7 @@ class Todo(db.Model):
        
     def __repr__(self):
         return f"{self.sno} - {self.title} - {self.desc}"
+
 
 # create database
 # db instance is available only when app_context is available
